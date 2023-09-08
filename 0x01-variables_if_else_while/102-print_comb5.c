61b00496d37869a4aa017408002c115c1a14b88d
Officialdeemmy <adenijiademolaafis@gmail.com>
2023-09-08 15:07:46 -0700

    k

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int i = '0';
+	int j = '0';
+
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = 0; j <= 99; j++)
+		{
+			if (i < j)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
