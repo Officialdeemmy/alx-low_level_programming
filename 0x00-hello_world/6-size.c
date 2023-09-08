8362f6c1063f9ca56e3111a8ad811b99dc626791
Officialdeemmy <adenijiademolaafis@gmail.com>
2023-09-07 17:03:00 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,14 +7,14 @@
  */
 
 int main(void)
-{	
-	
+{
+
 	char a;
 	int b;
 	long int c;
 	long long d;
 	float f;
-	
+
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
@@ -22,4 +22,3 @@ int main(void)
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
-~     
