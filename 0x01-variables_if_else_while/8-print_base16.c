8730b0304c28eb1a20855486acdcad58c2a4ecf0
Officialdeemmy <adenijiademolaafis@gmail.com>
2023-09-08 12:03:42 -0700

    8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,14 +4,13 @@
  *
  * Return: returns 0
  */
-
 int main(void)
 {
 	int i;
 	char j;
 
-	for (i = 0; i < 10; i++)
-	       putchar(i + '0');
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
 	for (j = 'a'; j <= 'f'; j++)
 		putchar(j);
 	putchar('\n');
