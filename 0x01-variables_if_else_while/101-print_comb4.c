66102ed128db1782d3311e1d6af60151984e2190
Officialdeemmy <adenijiademolaafis@gmail.com>
2023-09-08 14:24:58 -0700

    101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int i = '0';
+	int j = '0';
+	int k = '0';
+	
+	for (k = '0'; k <= '9'; k++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			for (i = '0'; i <= '9'; i++)
+			{
+				if (!((i == j) || (j == k) || (j > i) || (k > j)))
+                        	{
+					putchar(k);
+					putchar(j);
+					putchar(i);
+					if (!(i == '9' && j == '8' && k == '7'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+        }
+        putchar ('\n');
+        return (0);
+}
