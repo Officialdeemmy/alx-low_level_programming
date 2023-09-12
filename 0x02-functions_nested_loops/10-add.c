cdbf6f7d4961fd6409c3fa1f49a854d53bc1b079
Officialdeemmy <adenijiademolaafis@gmail.com>
2023-09-12 11:06:00 -0700

    1

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - function that adds two numbers
+ * @i: parameter 1
+ * @j: paramter 2
+ * Return: result
+ */
+
+int add(int i, int j)
+{
+	int result = i + j;
+	return (result);
+}
