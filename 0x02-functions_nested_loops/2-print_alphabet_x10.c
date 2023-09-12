f569074247950654e6d6240e8330672ca6a482ed
Officialdeemmy <adenijiademolaafis@gmail.com>
2023-09-12 07:21:47 -0700

    1

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,16 +7,15 @@
 
 void print_alphabet_x10(void)
 {
-	int i = 0;
+	int i;
 	char j;
 
-	while (i <= 10)
+	for (i = 0; i <= 10; i++)
 	{
 		for (j = 'a'; j <= 'z'; j++)
 		{
 			_putchar(j);
 		}
 		_putchar('\n');
-		i++;
 	}
 }
