dd9147ba2aa2267c3fd9866c00e4ef8c5351c6b6
Officialdeemmy <adenijiademolaafis@gmail.com>
2023-09-12 15:11:19 -0700

    103

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - sum of even fibbonacci
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int limit = 4000000, num1 = 1, num2 = 2, next, sum = 2, i;
+		
+	while (next <= limit)
+	{
+		next = num1 + num2;
+		if (next % 2 == 0)
+			sum = sum + next;
+		num1 = num2;
+		num2 = next;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
