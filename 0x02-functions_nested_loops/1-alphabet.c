9a91127fd5affb3bd0629b664deedbe170dd884e
Officialdeemmy <adenijiademolaafis@gmail.com>
2023-09-12 06:32:36 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - check the code
+ *
+ * Return: always 0
+ */
+
+void print_alphabet(void)
+{
+	char i = 'a';
+
+	while (i <= 'z')
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
