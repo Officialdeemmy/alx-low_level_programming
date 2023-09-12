7ff7d8e6ae9e380e857876e42f49159e3106bde5
Officialdeemmy <adenijiademolaafis@gmail.com>
2023-09-12 08:41:29 -0700

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * abs - fuction name
+ * @i: function parameter
+ * Return: -a or a
+ */
+
+int _abs(int i)
+{
+	if (i < 0)
+		return(-i);
+	else if (i >= 0)
+		return (i);
+
+	return (0);
+}
