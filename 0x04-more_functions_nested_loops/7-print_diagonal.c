4c18c600f38d8a87be5738e7ea742ab62de2318f
Officialdeemmy <adenijiademolaafis@gmail.com>
2023-09-14 06:54:59 -0700

    1

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,8 +8,11 @@ void print_diagonal(int n)
 	{
 		for (i = 0; i < n; i++)
 		{
-			for (j = 0; j <= i; j++)
-				_putchar(' ');
+			if ( i > 0)
+			{
+				for (j = 0; j <= i; j++)
+					_putchar(' ');
+			}
 			_putchar('\\');
 			_putchar('\n');
 		}
