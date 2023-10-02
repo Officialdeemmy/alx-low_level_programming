732afc7f016f9a901536b5b6a173e8f023c1314e
Officialdeemmy <adenijiademolaafis@gmail.com>
2023-10-02 15:43:01 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print its program name
+ * @argc: arg count
+ * @argv: arg vector
+ * Return: 0
+ */
+
+int main(int __attribute__((unused)) argc,char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
