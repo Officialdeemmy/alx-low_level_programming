6f2f2b568541510c8bf4f52f834aaa4765311526
Officialdeemmy <adenijiademolaafis@gmail.com>
2023-10-02 17:25:39 -0700

    3

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -23,21 +23,23 @@ int main(int argc, char *argv[])
 
 	if (cents < 0)
 		printf("%d\n", 0);
-
-	while (cents > 0)
+	else	
 	{
-		if (cents >= 25)
-			cents = cents - 25;
-		else if (cents >= 10)
-			cents = cents - 10;
-		else if (cents >= 5)
-			cents = cents - 5;
-		else if (cents >= 2)
-			cents = cents - 2;
-		else if (cents >= 1)
-			cents = cents - 1;
-		coins++;
+		while (cents > 0)
+		{
+			if (cents >= 25)
+				cents = cents - 25;
+			else if (cents >= 10)
+				cents = cents - 10;
+			else if (cents >= 5)
+				cents = cents - 5;
+			else if (cents >= 2)
+				cents = cents - 2;
+			else if (cents >= 1)
+				cents = cents - 1;
+			coins++;
+		}
+		printf("%d\n", coins);
 	}
-	printf("%d\n", coins);
 	return (0);
 }
