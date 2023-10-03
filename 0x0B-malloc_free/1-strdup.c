06abddec8c9ccdb313a40746821bbad2f4ce3ea2
Officialdeemmy <adenijiademolaafis@gmail.com>
2023-10-03 04:29:14 -0700

    1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strdup - copies string to new space
+ * @str: string to copy
+ * Retutn: pointer to new array
+ */
+
+char *_strdup(char *str)
+{
+	int i;
+	char *cpy;
+	int count = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i != '\0'; i++)
+		count++;
+
+	cpy = malloc(sizeof(char) * count + 1);
+
+	if (cpy == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		cpy[i] = str[i];
+
+	return (cpy);
+}
