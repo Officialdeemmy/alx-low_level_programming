c0acbde5d9fd9913bb994217349178f4c490c0c6
Officialdeemmy <adenijiademolaafis@gmail.com>
2023-10-07 14:35:22 -0700

    2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,13 +19,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
 	for (i = 0; s1[i] != '\0'; i++)
 		s1len++;
 	for (i = 0; s2[i] != '\0'; i++)
 		s2len++;
 
-	output = malloc(sizeof(char) +  (s1len + n) + 1);
+	output = malloc(sizeof(char) * (s1len + n) + 1);
 	if (output == NULL)
 		return (NULL);
 	if (n >= s2len)
