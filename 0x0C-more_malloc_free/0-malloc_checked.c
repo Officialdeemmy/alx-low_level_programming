b9e9454bb920ec287dc08987a99c157f170c9eb8
Officialdeemmy <adenijiademolaafis@gmail.com>
2023-10-07 13:24:34 -0700

    2

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,11 +2,17 @@
 
 /**
  * malloc_checked - allocates memory
- * @b: the value
+ * @b:size of memory
  * Return: pointer to the allocated memory
  */
 
 void *malloc_checked(unsigned int b)
 {
+	char *output;
 
+	output = malloc(b);
+	if (output == NULL)
+		exit(98);
+	else
+		return (output);
 }
