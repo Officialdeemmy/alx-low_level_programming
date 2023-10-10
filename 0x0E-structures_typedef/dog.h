4d467b83b1bada82660b8b810b424c07500125b0
Officialdeemmy <adenijiademolaafis@gmail.com>
2023-10-10 08:47:21 -0700

    1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,8 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdio.h>
+
 /**
  * struct dog - dog structure
  * @name: name of dog
@@ -14,5 +16,6 @@ struct dog
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
