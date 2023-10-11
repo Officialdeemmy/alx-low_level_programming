81e113851ed41a0ed3dc5ea4ad8649353f137d8d
Officialdeemmy <adenijiademolaafis@gmail.com>
2023-10-11 14:44:54 -0700

    5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * free_dog - frees dog
+ * @d: the structure
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
