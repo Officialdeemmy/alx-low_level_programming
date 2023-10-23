7c6f7f5720b159f5215955757061481298120f7e
Officialdeemmy <adenijiademolaafis@gmail.com>
2023-10-23 16:12:55 -0700

    0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,12 +1,21 @@
-#include "main.h"
+#include "lists.h"
 
 /**
  * print_listint - print all rlrments of a list
- * @h: arg
+ * @h: head
  * Return: number of nodes
  */
 
 size_t print_listint(const listint_t *h)
 {
+	size_t len;
 
+	if (h == NULL)
+		return (0);
+	for (len = 0; h != NULL; len++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (len);
 }
